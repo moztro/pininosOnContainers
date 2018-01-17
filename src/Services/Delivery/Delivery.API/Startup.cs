013daf167473692eb372e5271c67d0550298a277
Daniel Uribe Ayvar <danieluribeayvar@gmail.com>
2018-01-17 12:15:19 -0600

    docker setup for build and run

--- src/Services/Delivery/Delivery.API/Startup.cs
+++ src/Services/Delivery/Delivery.API/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Catalog.API
+namespace Delivery.API
 {
     public class Startup
     {
