fef1e7a2523a012c2784149b84de8ca5ab1f3b2d
Daniel Uribe Ayvar <danieluribeayvar@gmail.com>
2018-01-17 15:54:32 -0600

    updated folder structure, added web and mobile projects

---
+++ src/Mobile/pininosApp/pininosApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using pininosApp.Models;
+
+namespace pininosApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
