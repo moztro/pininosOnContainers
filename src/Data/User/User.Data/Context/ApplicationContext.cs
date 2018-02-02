b619c82c811864f512cd5d0b90203441d2df8457
Fernando Canul Centeno <fernando.canul@nearshoretechnology.com>
2018-02-02 16:17:39 -0600

    fixed structure solution (for User entity) add User.API project add User.Service project add User.Data project add Models (for common models) project add Migrations for User.Data project add Swagger UI for User.API

---
+++ src/Data/User/User.Data/Context/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Containers.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Person> Persons { set; get; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            this.Database.Migrate();
+            this.Database.EnsureCreated();
+        }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);            
+        }
+
+    }
+}
